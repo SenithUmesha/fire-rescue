9ef297f776f30537c60eef39d04bdcd2c2b7f226
BlackEyedGhouL <34senith@gmail.com>
2022-01-01 09:41:46 +0530

    add splash screen

---
+++ app/src/main/java/com/blackeyedghoul/firefighters/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.blackeyedghoul.firefighters;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(SplashScreen.this, R.color.white));
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
